e20caa977b3ae791ac0d67f4d10c6cca751f8562
Nikos Pitsianis <pitsianis@yahoo.com>
2024-12-01 18:41:36 +0200

    testing against FFTW in fewer lines

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,50 +1,27 @@
-using CatabraFFT
-using Test
-
-@testset "Radix-2" begin
-    	n = 20
-	for i in 1:n
-    		x = [ComplexF64(rand(), rand()) for _ in 1:2^i]
-    		result = CatabraFFT.FFT(x)
-	end
-end
-
-@testset "Radix-3" begin
-    	n = 13
-    	for i in 1:n
-    		x = [ComplexF64(rand(), rand()) for _ in 1:3^i]
-    		result = CatabraFFT.FFT(x)
-	end
-end
-
-@testset "Radix-5" begin
-    	n =10
-	for i in 1:n
-    		x = [ComplexF64(rand(), rand()) for _ in 1:5^i]
-    		result = CatabraFFT.FFT(x)
-	end
-end
-
-@testset "Radix-7" begin
-    n = 8
-    for i in 1:n
-    		x = [ComplexF64(rand(), rand()) for _ in 1:7^i]
-    		result = CatabraFFT.FFT(x)
-	end
-end
+using CatabraFFT, FFTW, Test
 
 @testset "Mixed-Radix" begin
-	n = 7
-	for i in 1:n
-    		x = [ComplexF64(rand(), rand()) for _ in 1:10^i]
+	for n in 10 .^(1:7)
+    	x = randn(ComplexF64,n)
 		result = CatabraFFT.FFT(x)
+        @test result ≈ FFTW.fft(x)
 	end
 end
 
 @testset "Rader's FFT" begin
-	n = 1721
 	for n in [11,13,17,19, 43, 89, 1721]
-    		x = [ComplexF64(rand(), rand()) for _ in 1:n]
+        x = randn(ComplexF64,n)
 		result = CatabraFFT.FFT(x)
+        @test result ≈ FFTW.fft(x)
 	end
+end
+
+for (b,maxexp) in [(2,20), (3,13), (5,10), (7,8)]
+    @testset "Radix-$b" begin
+        for n in b.^(1:maxexp)
+            x = randn(ComplexF64,n)
+            result = CatabraFFT.FFT(x)
+            @test result ≈ FFTW.fft(x)
+        end
+    end
 end
