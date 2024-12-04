5e7b5abd0f32085577ed01beaf749ac11323c9dc
Nikos Pitsianis <pitsianis@yahoo.com>
2024-12-04 16:47:23 +0200

    group tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,37 @@
 using CatabraFFT, FFTW, Test
 
 
-for (transformFunction, referenceTransform, transformationType) in 
-        [(CatabraFFT.fft, FFTW.fft, "Forward"), (CatabraFFT.ifft, FFTW.ifft, "Inverse")]
+for (transformFunction, referenceTransform, transformationType) in
+    [(CatabraFFT.fft, FFTW.fft, "Forward"), (CatabraFFT.ifft, FFTW.ifft, "Inverse")]
     for CType in [ComplexF64, ComplexF32] #ComplexF16
-        @testset "Mixed-Radix $transformationType FFT $CType" begin
-            for n in 10 .^ (1:7)
-                x = randn(CType, n)
-                result = transformFunction(x)
-                @test result ≈ referenceTransform(x)
-            end
-        end
+        @testset "$CType $transformationType FFT" begin
 
-        @testset "Rader's $transformationType FFT $CType" begin
-            for n in [11, 13, 17, 19, 43, 89, 1721]
-                x = randn(CType, n)
-                result = transformFunction(x)
-                @test result ≈ referenceTransform(x)
-            end
-        end
-
-        for (b, maxexp) in [(2, 20), (3, 13), (5, 10), (7, 8)]
-            @testset "Radix-$b $transformationType FFT $CType" begin
-                for n in b .^ (1:maxexp)
+            @testset "Mixed-Radix $transformationType FFT $CType" begin
+                for n in 10 .^ (1:7)
                     x = randn(CType, n)
                     result = transformFunction(x)
                     @test result ≈ referenceTransform(x)
                 end
             end
-        end
 
+            @testset "Rader's $transformationType FFT $CType" begin
+                for n in [11, 13, 17, 19, 43, 89, 1721]
+                    x = randn(CType, n)
+                    result = transformFunction(x)
+                    @test result ≈ referenceTransform(x)
+                end
+            end
+
+            for (b, maxexp) in [(2, 20), (3, 13), (5, 10), (7, 8)]
+                @testset "Radix-$b $transformationType FFT $CType" begin
+                    for n in b .^ (1:maxexp)
+                        x = randn(CType, n)
+                        result = transformFunction(x)
+                        @test result ≈ referenceTransform(x)
+                    end
+                end
+            end
+
+        end
     end
 end
