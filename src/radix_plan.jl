4389c6c098486c70124c02c6682f84da29cd6149
e-kyriakos <ekyriakos@X1.localdomain>
2025-10-05 14:12:08 +0300

    TODO fix layred kernel bug

--- src/radix_plan.jl
+++ src/radix_plan.jl
@@ -59,6 +59,7 @@ function create_all_radix_plans(n::Int, valid_radices::Vector{Int}, ::Type{T}) w
     end
     
     filtered = filter(is_valid, decompositions)
+    #filtered = [[2,2,2]]
     @show filtered
     return [create_radix_plan_from_decomposition(n, decomp, T) for decomp in filtered]
 end
